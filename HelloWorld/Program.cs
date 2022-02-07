d0f4fbabffe1aece5af890bd7067e89d929e6fc8
Yaseen <fantasticyaseenshariff@gmail.com>
2022-02-07 16:43:29 +0530

    Update program.cs

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 
-namespace HelloWorld
+namespace HelloCSharp
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello C Sharp!");
         }
     }
 }
